13229ee2224c60d337612a02a0431031e2f8c86a
mivo <michel.vorwieger@outlook.de>
2021-08-25 17:36:11 +0200

    init mezzio project with dockerfiles

---
+++ src/App/src/Handler/CarPageHandler.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Handler;
+
+use Laminas\Diactoros\Response\HtmlResponse;
+use Mezzio\Template\TemplateRendererInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class CarPageHandler implements RequestHandlerInterface
+{
+    private TemplateRendererInterface $renderer;
+
+    public function __construct(TemplateRendererInterface $renderer)
+    {
+        $this->renderer = $renderer;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        return new HtmlResponse($this->renderer->render('app::car-page'));
+        // TODO: Implement handle() method.
+    }
+}
---
+++ src/App/src/Handler/CarPageHandlerFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Handler;
+
+use Mezzio\Router\RouterInterface;
+use Mezzio\Template\TemplateRendererInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+use function get_class;
+
+class CarPageHandlerFactory
+{
+    public function __invoke(ContainerInterface $container): RequestHandlerInterface
+    {
+        $template = $container->has(TemplateRendererInterface::class)
+            ? $container->get(TemplateRendererInterface::class)
+            : null;
+
+        return new CarPageHandler($template);
+    }
+}
